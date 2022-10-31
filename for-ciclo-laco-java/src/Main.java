9d221504f15e3e4420a95d8ad47822524176ff02
Flitzso <106411702+flitzso@users.noreply.github.com>
2022-10-31 15:43:10 -0300

    Add files via upload

---
+++ for-ciclo-laco-java/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+// Precisa ter uam variavel indicando i = 0 ela determina aonde começa
+        for(int i = 0; i < 100; ++i){
+
+            System.out.println(i);
+        }
+    }
+}
